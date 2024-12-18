2a1da51405d954c207e9a09692f2e5fd9f907735
heejunida <qazwsx05723@gmail.com>
2024-12-18 17:46:57 +0900

    12월 18일 오후 5시 46분
    상품 매퍼 수정

--- codetouch/src/main/java/com/tagmaster/codetouch/mapper/ProductMapper.java
+++ codetouch/src/main/java/com/tagmaster/codetouch/mapper/ProductMapper.java
@@ -15,13 +15,19 @@ public interface ProductMapper {
     void updateProduct(ProductDTO dto);
     //삭제
     @Delete("DELETE FROM product WHERE pd_id=#{pd_id}")
-    void deleteProduct(ProductDTO dto);
+    int deleteProduct(ProductDTO dto);
     //특정 상품에 대한 모든 정보 출력
     @Select("SELECT site_id, name, category, price, image, description FROM product WHERE pd_id=#{pd_id}" )
     List<ProductDTO> findAllById(int product_id);
     //특정 상품에 대한 이름과 가격 출력
     @Select("SELECT name, price FROM product WHERE pd_id=#{pd_id}")
     ProductDTO findProductById(int product_id);
+    //높은 가격순 상품 리스트 출력
+    @Select("SELECT site_id, name, category, price, image, description FROM product ORDER BY price ASC")
+    List<ProductDTO> findAllProductsByHighPrice();
+    //낮은 가격순 상품 리스트 출력
+    @Select("SELECT site_id, name, category, price, image, description FROM product ORDER BY price DESC")
+    List<ProductDTO> findAllProductsByLowPrice();
     //특정 카테고리에 속해 있는 상품 정보 출력
     @Select("SELECT name, price, price, image, description FROM product WHERE category=#{category}")
     ProductDTO findProductByCategory(int category);
