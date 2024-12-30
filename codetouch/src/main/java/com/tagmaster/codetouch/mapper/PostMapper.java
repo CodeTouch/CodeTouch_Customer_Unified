81c1622bcb9498a991a14a693e5b815c1f71d306
heejunida <qazwsx05723@gmail.com>
2024-12-30 22:04:18 +0900

    12월 30일 오후 10시 04분
    게시물 매퍼 수정

--- codetouch/src/main/java/com/tagmaster/codetouch/mapper/PostMapper.java
+++ codetouch/src/main/java/com/tagmaster/codetouch/mapper/PostMapper.java
@@ -13,7 +13,7 @@ public interface PostMapper {
     int insertPost(PostDTO dto);
 
     //게시글 아이디로 게시글 찾기
-    @Select("SELECT u.email AS email, p.content, p.image, p.rating FROM post p JOIN user u on u.user_id = p.user_id WHERE p.post_id=#{post_id} and p.type='문의'")
+    @Select("SELECT u.email, p.title, p.content, p.image, p.rating FROM post p JOIN user u on u.user_id = p.user_id WHERE p.post_id=#{post_id} and p.type='문의'")
     PostDTO getPostById(int post_id);
 
     //삭제
@@ -27,7 +27,7 @@ public interface PostMapper {
     List<PostDTO> getAllPosts(int site_id, String type, int limit, int offset );
 
 
-    @Select("SELECT p.pd_id, p.user_id, p.type, p.content, p.image, p.rating, p.create_at, " +
+    @Select("SELECT p.title, p.type, p.content, p.image, p.rating, p.create_at, " +
             "       pd.name AS product_name, u.email AS user_email " +
             "FROM post p " +
             "JOIN product pd ON p.pd_id = pd.pd_id " +
@@ -37,7 +37,7 @@ public interface PostMapper {
     List<PayHistoryDetailsDTO> getPostsBySiteIdAndType(int site_id, String type);
 
     //5개 뽑기
-    @Select("SELECT p.pd_id, p.user_id, p.type, p.content, p.image, p.rating, p.create_at, " +
+    @Select("SELECT p.title, p.type, p.content, p.image, p.rating, p.create_at, " +
             "       pd.name AS product_name, u.email AS user_email " +
             "FROM post p " +
             "JOIN product pd ON p.pd_id = pd.pd_id " +
@@ -48,7 +48,7 @@ public interface PostMapper {
             "LIMIT 5")
     List<PayHistoryDetailsDTO> getTop5PostsBySiteIdAndType(int site_id, String type);
     // 10개 뽑기
-    @Select("SELECT p.pd_id, p.user_id, p.type, p.content, p.image, p.rating, p.create_at, " +
+    @Select("SELECT p.title, p.type, p.content, p.image, p.rating, p.create_at, " +
             "       pd.name AS product_name, u.email AS user_email " +
             "FROM post p " +
             "JOIN product pd ON p.pd_id = pd.pd_id " +
@@ -59,7 +59,7 @@ public interface PostMapper {
             "LIMIT 10")
     List<PayHistoryDetailsDTO> getTop10PostsBySiteIdAndType(int site_id, String type);
 
-    @Select("SELECT p.pd_id, p.user_id, p.type, p.content, p.image, p.rating, p.create_at, " +
+    @Select("SELECT p.title, p.type, p.content, p.image, p.rating, p.create_at, " +
             "       pd.name AS product_name, u.email AS user_email " +
             "FROM post p " +
             "JOIN product pd ON p.pd_id = pd.pd_id " +
@@ -74,15 +74,15 @@ public interface PostMapper {
 //   List<PostDTO> getPostsByUserId(int site_id, String type, int user_id);
 
     //상품 아이디로 게시글 찾기
-    @Select("SELECT pd_id, content, image, rating FROM post WHERE pd_id=#{pd_id} and type='문의'")
+    @Select("SELECT pd.name, u.email, p.title, p.content, p.image, p.rating FROM post p JOIN user u ON p.user_id=u.user_id JOIN product pd ON p.pd_id=pd.pd_id WHERE pd_id=#{pd_id} and type='문의'")
     List<PostDTO> getPostsByProductId(int pd_id, String type);
 
     //내용에 키워드로 검색해 찾기
-    @Select("SELECT u.email, title, content, image, create_at FROM post p JOIN user u ON p.user_id=u.user_id " +
+    @Select("SELECT u.email, p.title, p.content, p.image, p.create_at FROM post p JOIN user u ON p.user_id=u.user_id " +
             "WHERE p.site_id = #{site_id} AND p.type = '문의' AND content LIKE CONCAT('%', #{content}, '%')")
     List<PostDTO> getPostsByKeyword(PostSearchDTO dto, int limit , int offset);
   
-    @Select("SELECT pd_id, user_id, type, content, image, rating, create_at FROM post WHERE site_id=#{site_id} AND type='후기' AND content LIKE CONCAT ('%', #{content}, '%')")
+    @Select("SELECT u.email, p.type, p.content, p.image, p.rating, p.create_at FROM post p JOIN user u ON p.user_id=u.user_id WHERE site_id=#{site_id} AND type='후기' AND content LIKE CONCAT ('%', #{content}, '%')")
     List<PayHistoryDetailsDTO> getReviewByKeyword(PostSearchDTO dto);
     //별점 높은 순으로 게시글 가져오기
 //    @Select("SELECT pd_id, user_id, type, content, image, title, rating, pd_image, create_at FROM post WHERE pd_id=#{pd_id} ORDER BY rating DESC")
@@ -95,6 +95,5 @@ public interface PostMapper {
 //    @Insert("INSERT INTO post (pd_id, site_id, user_id, type, content, image, rating, create_at) VALUES (#{pd_id}, #{site_id}, #{user_id}, #{type}, #{content}, #{image}, #{rating}, now())")
 //    int insertReview(ReviewDTO reviewDTO);
 
-//join 으로 이메일 뽑아오기 //todo
 
 }
